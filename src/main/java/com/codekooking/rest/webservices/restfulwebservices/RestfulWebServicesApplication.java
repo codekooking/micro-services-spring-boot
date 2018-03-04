3aadcd24a5ef54a93cb2d58f914df22a7d47d829
Bill PC <billpc@gmail.com>
2018-03-05 06:29:10 +0700

    Initial Restful

---
+++ src/main/java/com/codekooking/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.codekooking.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+}
