c2169bf9702da20381df9ff07547afc8b35817aa
Thuan Nguyen <thuan.nq@sutrixsolutions.com>
2018-03-09 10:40:47 +0700

    Adding Error Handler

--- src/main/java/com/codekooking/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/codekooking/rest/webservices/restfulwebservices/user/User.java
@@ -2,48 +2,56 @@ package com.codekooking.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
-	
-	private Integer id;
-	
-	private String name;
-	
-	private Date dateOfBirth;
 
-	public User(Integer id, String name, Date dateOfBirth) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.dateOfBirth = dateOfBirth;
-	}
+    private Integer id;
 
-	public Integer getId() {
-		return id;
-	}
+    @Size(min=2, message="Name should have atleast 2 characters")
+    private String name;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @Past
+    private Date dateOfBirth;
 
-	public String getName() {
-		return name;
-	}
+    protected User() {
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public User(Integer id, String name, Date dateOfBirth) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
 
-	public Date getDateOfBirth() {
-		return dateOfBirth;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setDateOfBirth(Date dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + "]";
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + "]";
+    }
 
 }
