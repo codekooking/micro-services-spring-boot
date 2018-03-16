7187a6760b841eca3de3653a39e3df02691bd7b4
Thuan Nguyen <thuan.nq@sutrixsolutions.com>
2018-03-16 16:46:14 +0700

    Update services

--- micro-services/netflix-zuul-api-gateway-server/src/main/java/com/codekooking/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
+++ micro-services/netflix-zuul-api-gateway-server/src/main/java/com/codekooking/microservices/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 public class NetflixZuulApiGatewayServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+    }
 }
