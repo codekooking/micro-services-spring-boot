f6bde2377acada888c02d3f76c790150b0bb104c
Thuan Nguyen <thuan.nq@sutrixsolutions.com>
2018-04-05 15:40:34 +0700

    Adding Zipkin server

--- micro-services/currency-exchange-service/src/main/java/com/codekooking/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ micro-services/currency-exchange-service/src/main/java/com/codekooking/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.codekooking.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import com.codekooking.microservices.currencyexchangeservice.bean.ExchangeValueR
 
 @RestController
 public class CurrencyExchangeController {
+    
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private Environment environment;
@@ -24,6 +28,8 @@ public class CurrencyExchangeController {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        
+        logger.info("{}", exchangeValue);
 
         return exchangeValue;
 
