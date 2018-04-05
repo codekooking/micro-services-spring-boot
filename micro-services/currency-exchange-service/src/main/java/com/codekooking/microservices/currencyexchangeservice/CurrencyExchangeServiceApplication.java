f0caeefe618a433100fda063eb7b6a5cd3b27047
Thuan Nguyen <thuan.nq@sutrixsolutions.com>
2018-04-05 15:40:34 +0700

    Adding Zipkin server

--- micro-services/currency-exchange-service/src/main/java/com/codekooking/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ micro-services/currency-exchange-service/src/main/java/com/codekooking/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import brave.sampler.Sampler;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class CurrencyExchangeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+    }
+    
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
